f0a0d725d0db47270fe0cf117e8675ec1e49d15e
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2020-12-22 15:48:23 +0100

    completed-no_bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati
-
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
-
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 var nCpu = [];
@@ -49,10 +45,9 @@ console.log(nUser);
 function nRandom(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 // Seleziona una difficolta adeguata
 function diff(set) {
     do {
         set = parseInt(prompt('Scegli La difficoltà tra "0" , "1" , "2" '));
-    } while (set != 0 && set != 1 && set != 2) 
+    } while (set != 0 && set != 1 && set != 2);
 }
